9cb2556d8c8c7684b1b5613029d841bb3eb7ef9b
Azamat Omirtai <omirtajazamat@gmail.com>
2025-09-23 14:33:20 +0500

    [fix]//: cleanup fonts and colors setup in layout, css and tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,17 +3,21 @@ import type { Config } from "tailwindcss"
 const config: Config = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       colors: {
-        primary: "var(--color-primary)"
-      }
+        primary: "var(--color-primary)",
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+      fontFamily: {
+        sans: ["var(--font-sans)", "sans-serif"],
+      },
     },
   },
   plugins: [],
 }
 
 export default config
-
